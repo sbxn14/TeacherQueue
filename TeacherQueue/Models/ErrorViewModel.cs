df80e43cb4b24995d7646439dc48af33f4bba06a
Sander Berntsen <sander1006@hotmail.nl>
2019-06-18 13:22:17 +0200

    Add project files.

---
+++ TeacherQueue/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeacherQueue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
